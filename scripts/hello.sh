6c73ae738ada4ef7057057614c9eae9e8423d748
yuletak <yui@nttmcl.com>
2012-09-04 11:07:13 -0700

    more test reset author

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -2,6 +2,9 @@
 
 echo "Hello, world!"
 echo "You have just executed a script using Jenkins and Github!"
-echo "See Jenkins log for build info."
+echo "See Jenkins directory for build info."
 
+echo "We should now have build triggered by a push."
 echo "Timestamp:  "`date`
+
+echo "what more to do?"
